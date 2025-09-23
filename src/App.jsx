b45d402aea0ec8cc3ad67c2584fa9e4a4aba6948
Karim Ali <101140331+KariimAlii@users.noreply.github.com>
2025-09-23 20:41:24 +0300

    Show Error Page on navigating to an invalid route

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import Products from "./pages/Products.jsx";
 import Layout from "./pages/Layout.jsx";
+import Error from "./pages/Error.jsx";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <Layout />,
+        errorElement: <Error/>,
         children: [
             { path: '/', element: <Home /> },
             { path: '/products', element: <Products /> }
---
+++ src/pages/Error.jsx
@@ -0,0 +1,14 @@
+﻿import MainNavigation from "../components/MainNavigation.jsx";
+
+export default function Error() {
+    return (
+        <>
+            <MainNavigation/>
+            <main>
+                <h1>An error occured!</h1>
+                <p>Couldn't find this page!</p>
+            </main>
+        </>
+    )
+
+}
