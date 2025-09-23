925241861972641782a1cdc7855e708eacac7fdc
Karim Ali <101140331+KariimAlii@users.noreply.github.com>
2025-09-23 19:34:37 +0300

    Adding Multi Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import Home from "./pages/Home.jsx";
+import Products from "./pages/Products.jsx";
 
 const router = createBrowserRouter([
-    {path: '/', element: <Home /> }
+    {path: '/', element: <Home /> },
+    {path: '/products', element: <Products /> }
 ])
 export default function App() {
     return <RouterProvider router={router}/>;
---
+++ src/pages/Products.jsx
@@ -0,0 +1,5 @@
+﻿export default function Products() {
+    return (
+        <h1>Products Page</h1>
+    )
+}
