9ac993ff2f6ec663c381a31d6484a55d8ce93219
Karim Ali <101140331+KariimAlii@users.noreply.github.com>
2025-09-23 19:39:24 +0300

    Revert "Use the old way of configuring routes by html elements"
    This reverts commit 3adc6e732e33f74e7bd9822a6e7f03c6fa037f6f.

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,11 @@
-import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from "react-router-dom";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import Products from "./pages/Products.jsx";
 
-const routeDefinitions = createRoutesFromElements(
-    <Route>
-        <Route path="/" element={<Home />} />
-        <Route path="/products" element={<Products />} />
-    </Route>
-)
-const router = createBrowserRouter(routeDefinitions);
-
-// const router = createBrowserRouter([
-//     {path: '/', element: <Home /> },
-//     {path: '/products', element: <Products /> }
-// ])
+const router = createBrowserRouter([
+    {path: '/', element: <Home /> },
+    {path: '/products', element: <Products /> }
+])
 export default function App() {
     return <RouterProvider router={router}/>;
 }
